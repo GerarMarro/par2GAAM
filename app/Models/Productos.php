a10718cd0522bdbe4d81e3055edbc361a551ca2e
GerarMarro <gerardo.acevedo@catolica.edu.sv>
2021-04-28 22:25:31 -0600

    Parcial de Emergentes

---
+++ app/Models/Productos.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class Productos extends Eloquent
+{
+    protected $connection = 'mongodb';
+    protected $collection = 'productos';
+    protected $fillable = [
+        'codprod',
+        'nameprod',
+        'typeprod',
+        'costunit',
+        'cantexist'
+    ];
+}
